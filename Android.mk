e0d00e291b3f252ead8a0b9e8805f8c1fea20435
Diogo Ferreira <defer@cyanogenmod.com>
2011-08-31 18:40:07 +0100

    blade: Only inherit makefiles if building for blade
    This avoids generating blade-specific files on other
    builds.
    
    Change-Id: Ia9c06aaeb6bbe7e6da8d684babbe5c7a3539a4b6

--- Android.mk
+++ Android.mk
@@ -12,5 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-include $(call all-named-subdir-makefiles, recovery libaudio libril libcamera libgralloc libcopybit libsensors BladeParts)
+ifeq ($(TARGET_DEVICE),blade)
+    include $(call all-named-subdir-makefiles, recovery libaudio libril libcamera libgralloc libcopybit libsensors BladeParts)
+endif
